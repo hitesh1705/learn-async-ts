099236c75cc41e7dd55ef446e2fe1315db7f28ca
Hitesh Yarlagadda <hiteshyarlagadda17@gmail.com>
2024-10-20 11:45:11 -0400

    updated animation

--- src/quiz/animations/main.ts
+++ src/quiz/animations/main.ts
@@ -13,25 +13,21 @@ const alice10 = document.querySelector<HTMLElement>("#alice1");
 const alice20 = document.querySelector<HTMLElement>("#alice2");
 const alice30 = document.querySelector<HTMLElement>("#alice3");
 
-if(alice10 && alice20 && alice30) {
-  // Promise chain  
-  alice10.animate(aliceTumbling1, aliceTiming1).finished  
-    .then(() => {
-        return alice20
-                .animate(aliceTumbling1, aliceTiming1)
-                .finished;     
-    })
-    .then(() => {
-      return alice30
-              .animate(aliceTumbling1, aliceTiming1)
-              .finished;
-    })
-    .catch((err) => alert(`Error when promising ... ${err.message}`));
-}
-else{
-  console.warn("#alice not found");
+async function animateAlice() {
+  if (alice10 && alice20 && alice30) {
+    try {
+      await alice10.animate(aliceTumbling1, aliceTiming1).finished;
+      await alice20.animate(aliceTumbling1, aliceTiming1).finished;
+      await alice30.animate(aliceTumbling1, aliceTiming1).finished;
+    } catch (err) {
+      alert(`Error when animating: ${(err as Error).message}`);
+    }
+  } else {
+    console.warn("#alice element(s) not found");
+  }
 }
 
+animateAlice();
 // alice10
 //     .animate(aliceTumbling1, aliceTiming1)
 //     .finished
