8a8ed7bae25770666753949612e93bb24445465e
Hitesh Yarlagadda <hiteshyarlagadda17@gmail.com>
2024-10-20 11:09:05 -0400

    updated 2d array

--- src/quiz/promise-2darray.ts
+++ src/quiz/promise-2darray.ts
@@ -2,7 +2,9 @@ function sum2DArray(arr: number[][]): Promise<number> {
     return new Promise((resolve, reject) => {
         console.log('Sum called ... ');
         if(arr.length === 0) {
+            console.log('Rejecting empty array');
             reject('Cannot sum an empty array');
+            return;
         }
         setTimeout(() => {
             let sum = 0;
@@ -25,8 +27,12 @@ const array2D = [
     [7, 8, 9]
 ];
 
-const sumPromise1 = sum2DArray(array2D);
-console.log('sumPromise1:', sumPromise1);
+sum2DArray(array2D)
+    .then(sum => console.log('Sum of array2D:', sum))
+    .catch(error => console.error(error));
 
-const sumPromise2 = sum2DArray([]);
-console.log('sumPromise2:', sumPromise2);
+sum2DArray([])
+    .then(sum => console.log('Sum of empty array:', sum))
+    .catch(error => console.error('Error:', error));
+
+console.log('After sumPromise calls...');
